d53a0ddaf8f9605e6ba23ef475804910b8e627c2
Chris Dearman <chris@mips.com>
2011-11-17 18:35:42 -0800

    Allow config file to override HAVE_SYS_UIO_H for non NDK build
    Signed-off-by: Chris Dearman <chris@mips.com>

--- crypto/include/config.h
+++ crypto/include/config.h
@@ -86,6 +86,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <sys/uio.h> header file. */
+#undef HAVE_SYS_UIO_H
 #define HAVE_SYS_UIO_H 1
 
 /* Define to 1 if the system has the type `uint16_t'. */
